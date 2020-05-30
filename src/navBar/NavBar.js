d889f9a60168f0a7199e91842037205daf643339
devpuskar <puskar.ad@gmail.com>
2020-05-31 02:32:03 +0545

    added subnav

--- src/navBar/NavBar.js
+++ src/navBar/NavBar.js
@@ -4,14 +4,18 @@ import { SearchBar } from "../landingPage/SearchBar/SearchBar";
 
 import logo from "../assets/logo.png";
 import styles from "./NavBar.module.css";
+import { SubNav } from "./subNav/SubNav";
 
 export function NavBar() {
   return (
-    <div className={styles["navBar"]}>
-      <img src={logo} alt="logo" className={styles.logo} />
-      <SearchBar small/>
-      <button className={`button ${styles["nav-button"]}`}>Sign In</button>
-      <button className={`button ${styles["nav-button"]}`}>Sign Up</button>
+    <div>
+      <div className={styles["navBar"]}>
+        <img src={logo} alt="logo" className={styles.logo} />
+        <SearchBar small />
+        <button className={`button ${styles["nav-button"]}`}>Sign In</button>
+        <button className={`button ${styles["nav-button"]}`}>Sign Up</button>
+      </div>
+      <SubNav />
     </div>
   );
 }
---
+++ src/navBar/subNav/SubNav.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import styles from "./SubNav.module.css";
+import { SubNavItem } from "./subNavItem/SubNavItem";
+
+export function SubNav() {
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["sub-nav"]}>
+        <div>
+          <SubNavItem label="Restaurants" icon="fa-utensils" />
+          <SubNavItem label="Home Services" icon="fa-home" />
+          <SubNavItem label="Delivery" icon="fa-truck" />
+          <SubNavItem label="More" icon="fa-info-circle" showRightBorder />
+        </div>
+        <div>
+          <button
+            className={`button ${styles["subNab-btn"]} ${styles["omit-right-border"]}`}
+          >
+            <span className="icon">
+              <i className="fas fa-pen"></i>
+            </span>
+            <span>Write a Review</span>
+          </button>
+          <button
+            className={`button ${styles["subNab-btn"]} ${styles["right-border"]}`}
+          >
+            <span className="icon">
+              <i className="fas fa-hotel"></i>
+            </span>
+            <span>For Businesses</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
