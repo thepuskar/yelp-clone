3bcf1e971731decdf35dcce24e4c06591e2baf43
devpuskar <puskar.ad@gmail.com>
2020-05-31 03:31:54 +0545

    added search summery result

--- src/search/Search.js
+++ src/search/Search.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import styles from "./Search.module.css";
 import { NavBar } from "../navBar/NavBar";
+import { SearchResultSummery } from "./searchResultSummery/SearchReasultSummery";
 
 export function Search() {
   return (
     <div>
       <NavBar />
+      <SearchResultSummery />
     </div>
   );
 }
---
+++ src/search/searchResultSummery/SearchReasultSummery.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import styles from "./SearchResultSummery.module.css";
+
+export function SearchResultSummery() {
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["search-summery"]}>
+        <h2 className="subtitle">
+          <strong>Burgers</strong> in Berlin
+        </h2>
+        <p>Showing 1-20 out of 284 results</p>
+      </div>
+      <div className={styles.filter}>
+        <button className="button">
+          <span className="icon">
+            <i className="fas fa-sliders-h"></i>
+          </span>
+          <span>All filters</span>
+        </button>
+        <div class="buttons has-addons">
+          <button className="button">$</button>
+          <button className="button">$$</button>
+          <button className="button">$$$</button>
+          <button className="button">$$$$</button>
+        </div>
+        <button className="button">
+          <span className="icon">
+            <i className="fas fa-clock"></i>
+          </span>
+          <span>Open Now</span>
+        </button>
+        <button className="button">
+          <span className="icon">
+            <i className="fas fa-dollar-sign"></i>
+          </span>
+          <span>Cashback</span>
+        </button>
+      </div>
+    </div>
+  );
+}
