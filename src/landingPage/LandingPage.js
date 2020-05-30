369df26d088a13d67f217f179ea9cba24948e7bb
devpuskar <puskar.ad@gmail.com>
2020-05-30 16:48:33 +0545

    logo, favicon and search bar added

--- src/landingPage/LandingPage.js
+++ src/landingPage/LandingPage.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+import logo from "../assets/logo.png";
+
 import { TopNav } from "./TopNav/TopNav";
+
+import styles from "./LandingPage.module.css";
+import { SearchBar } from "./searchBar/SearchBar";
 
 export function LandingPage() {
   return (
     <di>
       <TopNav />
+      <img src={logo} alt="logo" className={styles.logo} />
+      <SearchBar />
     </di>
   );
 }
---
+++ src/landingPage/searchBar/SearchBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./SearchBar.module.css";
+
+export function SearchBar() {
+  return (
+    <div>
+      <div className="field has-addons">
+        <p className="control">
+          <button className="button is-static is-medium">Search</button>
+        </p>
+        <p className="control">
+          <input
+            className={`input is-medium ${styles["input-control"]}`}
+            type="text"
+            placeholder="burgers, barbers, spas, handymen"
+          />
+        </p>
+        <p className="control">
+          <button className="button is-static is-medium">NEAR</button>
+        </p>
+        <p className="control">
+          <input
+            className={`input is-medium ${styles["input-control"]}`}
+            type="text"
+            placeholder="where"
+          />
+        </p>
+      </div>
+    </div>
+  );
+}
