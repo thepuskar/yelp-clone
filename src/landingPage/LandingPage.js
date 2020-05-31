be8ae6e7811dbf494775a7ca7bd737101a26cd8b
devpuskar <puskar.ad@gmail.com>
2020-05-31 19:28:43 +0545

    background slideshow added

--- src/landingPage/LandingPage.js
+++ src/landingPage/LandingPage.js
@@ -7,16 +7,20 @@ import { TopNav } from "./TopNav/TopNav";
 import styles from "./LandingPage.module.css";
 import { SearchBar } from "./SearchBar/SearchBar";
 import { SearchSuggestion } from "./SearchSuggestion/SearchSuggestion";
+import { BackgroundSlideshow } from "./backgroundSlider/BackgroundSlider";
 
 export function LandingPage() {
   return (
     <div className={styles.landing}>
-      <TopNav />
-      <div className={styles["search-area"]}>
-        <img src={logo} alt="logo" className={styles.logo} />
-        <SearchBar />
-        <SearchSuggestion />
+      <div>
+        <TopNav />
+        <div className={styles["search-area"]}>
+          <img src={logo} alt="logo" className={styles.logo} />
+          <SearchBar />
+          <SearchSuggestion />
+        </div>
       </div>
+      <BackgroundSlideshow />
     </div>
   );
 }
